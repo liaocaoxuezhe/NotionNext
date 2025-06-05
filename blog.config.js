7c5855da85f995273a83ca6044a0928f702a8673
潦草学者 <48851266+liaocaoxuezhe@users.noreply.github.com>
2025-06-05 10:26:29 +0800

    Update vlog_Favicon

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   BIO: process.env.NEXT_PUBLIC_BIO || '阅读 思考 创作 感受', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.liaocaoxuezhe.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '生成式AI,效率工具,知识管理,个人博客,产品经理', // 网站关键词 英文逗号隔开
-  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
+  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || 'https://illegiuploadpics.oss-cn-beijing.aliyuncs.com/liaocao-avator.png', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
 
